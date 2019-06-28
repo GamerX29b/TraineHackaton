213670f5a8f7b0065ebba7fe01814765ecdadcdb
Sergei Eyrikh <eyrikh@lanit.ru>
2019-06-28 17:09:00 +0600

    17

--- src/main/java/com/example/TraineeHackathon/BaseClass/PersonUtils.java
+++ src/main/java/com/example/TraineeHackathon/BaseClass/PersonUtils.java
@@ -1,7 +1,5 @@
 package com.example.TraineeHackathon.BaseClass;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
 
@@ -10,15 +8,11 @@ import java.util.Date;
 public class PersonUtils {
 
 
-    private Logger logger = LoggerFactory.getLogger(this.getClass());
-    @Autowired
-    PersonRepository PersonRepository;
+    PersonRepository personRepository;
 
     public void personSave (Long id, String name, Date birthdate){
 
-        logger.info("Inserting -> {}", PersonRepository.save(new PersonBase(id, name, birthdate)));
+        personRepository.save(new PersonBase(id, name, birthdate));
     }
-
-
 
 }
