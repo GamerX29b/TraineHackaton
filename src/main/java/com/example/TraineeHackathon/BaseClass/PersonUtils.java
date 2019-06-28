ef60f3c10727450ad5bc9b356ff978d14e97a14a
Sergei Eyrikh <eyrikh@lanit.ru>
2019-06-28 18:44:55 +0600

    Test22

--- src/main/java/com/example/TraineeHackathon/BaseClass/PersonUtils.java
+++ src/main/java/com/example/TraineeHackathon/BaseClass/PersonUtils.java
@@ -1,10 +1,11 @@
 package com.example.TraineeHackathon.BaseClass;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 
 import java.util.Date;
-
+@Component
 public class PersonUtils {
     @Autowired
     PersonRepository personRepository;
