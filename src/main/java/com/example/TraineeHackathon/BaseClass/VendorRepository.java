5954964979babd21106d9960f1c517ac01050a37
Sergei Eyrikh <eyrikh@lanit.ru>
2019-07-03 15:38:20 +0600

    ТОЧНО РАБОТАЕТ

--- src/main/java/com/example/TraineeHackathon/BaseClass/VendorRepository.java
+++ src/main/java/com/example/TraineeHackathon/BaseClass/VendorRepository.java
@@ -1,9 +1,11 @@
 package com.example.TraineeHackathon.BaseClass;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 interface VendorRepository extends JpaRepository<VendorBase, Long> {
+    @Query(value = "SELECT COUNT (DISTINCT VENDOR_NAME) FROM VendorBase")
    Long countDistinctVendorNameBy();
 }
